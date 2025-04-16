dae71fab2356f3b5dd136b34bf90a741826534fb
isabella <iordosgoitia@utb.edu.co>
2025-04-16 11:02:21 -0500

    Update test movie_list file

--- backend/tests/test_movie_list.py
+++ backend/tests/test_movie_list.py
@@ -39,6 +39,27 @@ class TestMovieList(unittest.TestCase):
         self.assertEqual(self.movie_list.like_by, [2, 3], "The like_by list was not initialized correctly.")
         self.assertEqual(self.movie_list.saved_by, [4, 5], "The saved_by list was not initialized correctly.")
         self.assertEqual(self.movie_list.movies, [1001, 1002, 1003], "The movies list was not initialized correctly.")
+    
+    def test_setters_and_getters(self):
+        """
+        Verify all setters and getters are working.
+        """
+        self.movie_list.id = 2
+        self.movie_list.user_id = 20
+        self.movie_list.privacy = "private"
+        self.movie_list.list_name = "Sci-fi Hits"
+        self.movie_list.list_description = "Best sci-fi films ever"
+        self.movie_list.like_by = [1, 4]
+        self.movie_list.saved_by = [6]
+        self.movie_list.movies = [2001, 2002]
 
+        self.assertEqual(self.movie_list.id, 2)
+        self.assertEqual(self.movie_list.user_id, 20)
+        self.assertEqual(self.movie_list.privacy, "private")
+        self.assertEqual(self.movie_list.list_name, "Sci-fi Hits")
+        self.assertEqual(self.movie_list.list_description, "Best sci-fi films ever")
+        self.assertEqual(self.movie_list.like_by, [1, 4])
+        self.assertEqual(self.movie_list.saved_by, [6])
+        self.assertEqual(self.movie_list.movies, [2001, 2002])
 if __name__ == "__main__":
     unittest.main()
