e6256943e0cf1c98e0701273dcbad7b6a9c047e2
alexvirimi <alexvirimi@gmail.com>
2025-04-27 11:36:53 -0500

    Added <<Articles>> route

--- backend/api/main.py
+++ backend/api/main.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from backend.api.routes import (
     users,
-    profiles
+    profiles,
+    articles
 )
 
 api_router = APIRouter()
 api_router.include_router(users.router)
 api_router.include_router(profiles.router)
+api_router.include_router(articles.router)
